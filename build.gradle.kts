04f1d889484690454fddbfab3c4c6862415f3e02
Maria Tigina <maria.tigina@jetbrains.com>
2022-12-01 16:35:29 +0100

    Fix task runIde location

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ val platformVersion: String by project
 val platformType: String by project
 val platformDownloadSources: String by project
 val platformPlugins: String by project
-val pluginName: String by project
 
 allprojects {
     apply {
@@ -22,12 +21,6 @@ allprojects {
         plugin(rootProject.libs.plugins.gradle.ktlint.get().pluginId)
         plugin(rootProject.libs.plugins.jetbrains.intellij.get().pluginId)
     }
-
-//    dependencies {
-//        implementation(rootProject.libs.log4j.api)
-//        implementation(rootProject.libs.log4j.core)
-//        implementation(rootProject.libs.log4j.slf4j.impl)
-//    }
 
     repositories {
         mavenCentral()
@@ -50,7 +43,6 @@ allprojects {
     }
 
     tasks {
-
         withType<JavaCompile> {
             sourceCompatibility = "11"
             targetCompatibility = "11"
@@ -60,29 +52,5 @@ allprojects {
         }
         withType<org.jetbrains.intellij.tasks.BuildSearchableOptionsTask>()
             .forEach { it.enabled = false }
-
-        runIde {
-            // Define args for your application
-            val message: String? by project
-
-            args = listOfNotNull(
-                // Define your application starter command name
-                "ij-headless-greeting",
-                // Define args for your application
-                message?.let { "--message=$it" }
-            )
-
-            jvmArgs = listOf(
-                "-Djava.awt.headless=true",
-                "--add-exports",
-                "java.base/jdk.internal.vm=ALL-UNNAMED",
-                "-Djdk.module.illegalAccess.silent=true"
-            )
-
-            maxHeapSize = "32g"
-
-            standardInput = System.`in`
-            standardOutput = System.`out`
-        }
     }
 }
--- starters/build.gradle.kts
+++ starters/build.gradle.kts
@@ -4,3 +4,29 @@ version = rootProject.version
 dependencies {
     implementation(rootProject.libs.kotlin.argparser)
 }
+
+tasks {
+    runIde {
+        // Define args for your application
+        val message: String? by project
+
+        args = listOfNotNull(
+            // Define your application starter command name
+            "ij-headless-greeting",
+            // Define args for your application
+            message?.let { "--message=$it" }
+        )
+
+        jvmArgs = listOf(
+            "-Djava.awt.headless=true",
+            "--add-exports",
+            "java.base/jdk.internal.vm=ALL-UNNAMED",
+            "-Djdk.module.illegalAccess.silent=true"
+        )
+
+        maxHeapSize = "32g"
+
+        standardInput = System.`in`
+        standardOutput = System.`out`
+    }
+}
